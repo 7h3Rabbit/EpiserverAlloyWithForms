33442bace721673b41ba0d08fac1387e333793f4
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2020-04-12 22:25:53 +0200

    added example for IStaticWebIgnoreGenerateDynamically

--- Models/Pages/StandardPage.cs
+++ Models/Pages/StandardPage.cs
@@ -1,7 +1,12 @@
+using EPiServer;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
+using EPiServer.Forms.Implementation.Elements;
+using EPiServer.ServiceLocation;
+using StaticWebEpiserverPlugin.Interfaces;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace EpiserverAlloyWithForms.Models.Pages
 {
@@ -10,7 +15,7 @@ namespace EpiserverAlloyWithForms.Models.Pages
     /// </summary>
     [SiteContentType(GUID = "9CCC8A41-5C8C-4BE0-8E73-520FF3DE8267")]
     [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-standard.png")]
-    public class StandardPage : SitePageData
+    public class StandardPage : SitePageData, IStaticWebIgnoreGenerateDynamically
     {
         [Display(
             GroupName = SystemTabNames.Content,
@@ -22,5 +27,28 @@ namespace EpiserverAlloyWithForms.Models.Pages
             GroupName = SystemTabNames.Content,
             Order = 320)]
         public virtual ContentArea MainContentArea { get; set; }
+
+        public bool ShouldDeleteGenerated()
+        {
+            // This method is ONLY called if "ShouldGenerate" method returns false (And when this happens we ALWAYS want to remove generated page)
+            return true;
+        }
+
+        public bool ShouldGenerate()
+        {
+            var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            if (MainContentArea != null && MainContentArea.Items.Any())
+            {
+                foreach (var contentItem in MainContentArea.Items)
+                {
+                    FormContainerBlock formContainerBlock;
+                    if (contentLoader.TryGet<FormContainerBlock>(contentItem.ContentLink, out formContainerBlock))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
