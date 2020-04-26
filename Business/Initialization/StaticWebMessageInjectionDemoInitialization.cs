12fafcedd1fac55ec2acacdd91abcfb7e7d3d7c9
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2020-04-26 17:29:45 +0200

    fixed spelling in our example

--- Business/Initialization/StaticWebMessageInjectionDemoInitialization.cs
+++ Business/Initialization/StaticWebMessageInjectionDemoInitialization.cs
@@ -25,7 +25,7 @@ namespace EpiserverAlloyWithForms.Business.Initialization
              * But it was the only example I came up with at this time so here you go.... An example of how to use events when generating pages
              **/
 
-            var warningsMessage = "<div class=\"container navbar-fixed-bottom\"><div class=\"row\"><div class=\"alert alert-warning\" role=\"alert\">You are currently viewing a limited version of our website. One of the reasons for this could be because we currently have an abnormal amout of traffic.</div></div></div><br><br><br><br></body>";
+            var warningsMessage = "<div class=\"container navbar-fixed-bottom\"><div class=\"row\"><div class=\"alert alert-warning\" role=\"alert\">You are currently viewing a limited version of our website. One of the reasons for this could be because we currently have an abnormal amount of traffic.</div></div></div><br><br><br><br></body>";
             e.Content = e.Content.Replace("</body>", warningsMessage);
         }
 
